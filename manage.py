0db2a289b4dffbd6f467edf4d90ef1d3c2ea7b18
Shashank Rewankar <32234410+shasha98sr@users.noreply.github.com>
2023-07-19 12:58:42 -0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,6 @@ import sys
 
 from search.utils import stop_solr_server
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SemanticSearch.settings')
@@ -16,9 +15,8 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        )
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     try:
